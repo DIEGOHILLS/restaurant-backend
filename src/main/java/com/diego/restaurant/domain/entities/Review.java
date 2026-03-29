d2774952f7766ac20341e229f3be044b538eec21
DIEGOHILLS <125441047+DIEGOHILLS@users.noreply.github.com>
2026-03-29 21:35:30 +0200

    Update Review.java

--- src/main/java/com/diego/restaurant/domain/entities/Review.java
+++ src/main/java/com/diego/restaurant/domain/entities/Review.java
@@ -1,42 +1,33 @@
 package com.diego.restaurant.domain.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.elasticsearch.annotations.DateFormat;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
-@Data
-@AllArgsConstructor
+@Entity
+@Table(name = "reviews")
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Review {
 
-    @Field(type = FieldType.Keyword)
-    private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @Field(type = FieldType.Text)
+    private String title;
+
+    @Column(length = 2000)
     private String content;
 
-    @Field(type = FieldType.Integer)
     private Integer rating;
 
-    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
-    private LocalDateTime datePosted;
+    private LocalDateTime createdAt;
 
-    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
-    private LocalDateTime lastEdited;
-
-    @Field(type = FieldType.Nested)
-    private List<Photo> photos = new ArrayList<>();
-
-    @Field(type = FieldType.Nested)
-    private User writtenBy;
-
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
 }
