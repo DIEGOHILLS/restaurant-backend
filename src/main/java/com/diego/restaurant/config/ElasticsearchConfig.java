828d65f52c383c0385de83787eb56f4373431823
DIEGOHILLS <125441047+DIEGOHILLS@users.noreply.github.com>
2026-03-29 21:13:48 +0200

    Delete src/main/java/com/diego/restaurant/config/ElasticsearchConfig.java

--- src/main/java/com/diego/restaurant/config/ElasticsearchConfig.java
+++
@@ -1,9 +0,0 @@
-package com.diego.restaurant.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-
-@Configuration
-@EnableElasticsearchRepositories(basePackages = "com.diego.restaurant.repositories")
-public class ElasticsearchConfig {
-}
