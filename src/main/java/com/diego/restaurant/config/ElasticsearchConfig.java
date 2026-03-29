e956d831c11eb3bbd3f37d213af186a622e367a0
DIEGOHILLS <125441047+DIEGOHILLS@users.noreply.github.com>
2026-03-29 15:33:33 +0200

    Update ElasticsearchConfig.java

--- src/main/java/com/diego/restaurant/config/ElasticsearchConfig.java
+++ src/main/java/com/diego/restaurant/config/ElasticsearchConfig.java
@@ -1,41 +1,9 @@
 package com.diego.restaurant.config;
 
-import org.apache.http.HttpHost;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
-public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
-
-    @Override
-    @Bean
-    public RestHighLevelClient elasticsearchClient() {
-
-        final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-        credentialsProvider.setCredentials(
-                AuthScope.ANY,
-                new UsernamePasswordCredentials(
-                        System.getenv("ES_USERNAME"),
-                        System.getenv("ES_PASSWORD")
-                )
-        );
-
-        return new RestHighLevelClient(
-                RestClient.builder(
-                        new HttpHost(
-                                System.getenv("ES_HOST"),
-                                443,
-                                "https"
-                        )
-                ).setHttpClientConfigCallback(httpClientBuilder ->
-                        httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
-                )
-        );
-    }
+@EnableElasticsearchRepositories(basePackages = "com.diego.restaurant.repository")
+public class ElasticsearchConfig {
 }
