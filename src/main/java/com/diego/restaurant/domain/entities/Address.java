16c80d6a2ddff96ae0d9f64df79ddbd401673d70
DIEGOHILLS <125441047+DIEGOHILLS@users.noreply.github.com>
2026-03-29 21:49:16 +0200

    Refactor Address class for JPA integration
    Refactored Address class to use JPA annotations and removed Elasticsearch annotations.

--- src/main/java/com/diego/restaurant/domain/entities/Address.java
+++ src/main/java/com/diego/restaurant/domain/entities/Address.java
@@ -1,37 +1,19 @@
 package com.diego.restaurant.domain.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Data
-@AllArgsConstructor
+@Embeddable
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Address {
 
-    @Field(type = FieldType.Keyword)
-    private String streetNumber;
-
-    @Field(type = FieldType.Text)
-    private String streetName;
-
-    @Field(type = FieldType.Keyword)
-    private String unit;
-
-    @Field(type = FieldType.Keyword)
+    private String street;
     private String city;
-
-    @Field(type = FieldType.Keyword)
     private String state;
-
-    @Field(type = FieldType.Keyword)
-    private String postalCode;
-
-    @Field(type = FieldType.Keyword)
+    private String zipCode;
     private String country;
-
 }
